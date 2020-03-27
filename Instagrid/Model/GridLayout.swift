4252d511e4b5425ca14d8c1050da35543ae3faeb
Djiva Canessane <dcanessane@icloud.com>
2020-03-27 19:41:57 +0100

    fixed constraints and logo assets and create models

---
+++ Instagrid/Model/GridLayout.swift
@@ -0,0 +1,13 @@
+//
+//  Organization.swift
+//  Instagrid
+//
+//  Created by Djiva Canessane on 27/03/2020.
+//  Copyright © 2020 Djiva Canessane. All rights reserved.
+//
+
+import Foundation
+
+class GridLayout {
+    
+}
