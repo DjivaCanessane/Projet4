482d6bf0d48a4b6500d51d432f7c640230172045
Djiva Canessane <dcanessane@icloud.com>
2020-04-16 20:28:09 +0200

    add comments and modify var names and remove unnecessary spaces

--- Instagrid/Controller/ViewController.swift
+++ Instagrid/Controller/ViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-
     @IBOutlet weak var swipeArrowImage: UIImageView!
     @IBOutlet weak var swipeLabel: UILabel!
     @IBOutlet var rootView: UIView!
@@ -24,24 +23,25 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Select initialy the first button
+        // Select initially the first button
         buttonLayout1.setImage(UIImage(named: "Selected"), for: .normal)
         
         // Set mainLayout to first button's layout
         showMainLayout(buttonLayout1)
         
-        swipeGestureRecognier = UISwipeGestureRecognizer(target: self, action: #selector(onSwipe(_:)))
+        // Create swipeGestureRecognizer and add to mainView
+        swipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(onSwipe(_:)))
+        rootView.addGestureRecognizer(swipeGestureRecognizer)
         
-        
-        rootView.addGestureRecognizer(swipeGestureRecognier)
+        // Add notification when device change orientation
         NotificationCenter.default.addObserver(self, selector: #selector(resetOrientation), name: UIDevice.orientationDidChangeNotification, object: nil)
-
     }
     
     private var buttonBuffer: UIButton = UIButton()
-    private var swipeGestureRecognier: UISwipeGestureRecognizer!
+    private var swipeGestureRecognizer: UISwipeGestureRecognizer!
     private var transformAnimation: CGAffineTransform?
     
+    // Called by swipeGestureRecognizer
     @objc private func onSwipe(_ sender: UISwipeGestureRecognizer) {
         if sender.state == .ended {
             animateMainView()
@@ -56,10 +56,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             if isSwipped {
                 self.share()
             }
-            
         })
     }
     
+    // Converts UIView to UIImage
     private func image(with view: UIView) -> UIImage? {
         UIGraphicsBeginImageContextWithOptions(view.bounds.size, view.isOpaque, 0.0)
         defer { UIGraphicsEndImageContext() }
@@ -71,6 +71,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         return nil
     }
     
+    // Converts the mainView to UIImage then share with activityViewController
     private func share() {
         guard let image: UIImage = image(with: mainView) else { return }
         let activityViewController = UIActivityViewController(activityItems: [image], applicationActivities: [])
@@ -87,8 +88,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             }, completion: nil)
     }
     
+    // Update mainView according to the newly selected button
     @IBAction private func buttonActionLayoutTapped(_ sender: UIButton) {
-        print(#function)
         disableBackgroundImageBottomButtons()
         updateBackgroundImageForButton(sender)
         updateMainLayout(sender)
@@ -115,12 +116,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         }, completion: { (isComplete) in
             self.resetRows()
             self.showMainLayout(sender)
-            
         })
     }
     
     private func showMainLayout(_ sender: UIButton) {
-        
         self.mainView.transform = .identity
         UIView.animate(withDuration: 0.2, animations: {
             self.mainView.alpha = 1
@@ -128,44 +127,43 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         }, completion: nil)
     }
     
+    // add imagePickerButtons according to the selected buttonLayout
     private func setRows(_ sender: UIButton) {
-        let buttons: [UIButton]
+        let imagePickerbuttons: [UIButton]
         if sender == buttonLayout1 {
-            buttons = makeAddButton(count: 3)
-            topRow.addArrangedSubview(buttons[0])
-            bottomRow.addArrangedSubview(buttons[1])
-            bottomRow.addArrangedSubview(buttons[2])
+            imagePickerbuttons = makeImagePickerButton(count: 3)
+            topRow.addArrangedSubview(imagePickerbuttons[0])
+            bottomRow.addArrangedSubview(imagePickerbuttons[1])
+            bottomRow.addArrangedSubview(imagePickerbuttons[2])
             return
         }
         else if sender == buttonLayout2 {
-            buttons = makeAddButton(count: 3)
-            topRow.addArrangedSubview(buttons[0])
-            topRow.addArrangedSubview(buttons[1])
-            bottomRow.addArrangedSubview(buttons[2])
+            imagePickerbuttons = makeImagePickerButton(count: 3)
+            topRow.addArrangedSubview(imagePickerbuttons[0])
+            topRow.addArrangedSubview(imagePickerbuttons[1])
+            bottomRow.addArrangedSubview(imagePickerbuttons[2])
             return
         }
         else if sender == buttonLayout3 {
-            buttons = makeAddButton(count: 4)
-            topRow.addArrangedSubview(buttons[0])
-            topRow.addArrangedSubview(buttons[1])
-            bottomRow.addArrangedSubview(buttons[2])
-            bottomRow.addArrangedSubview(buttons[3])
+            imagePickerbuttons = makeImagePickerButton(count: 4)
+            topRow.addArrangedSubview(imagePickerbuttons[0])
+            topRow.addArrangedSubview(imagePickerbuttons[1])
+            bottomRow.addArrangedSubview(imagePickerbuttons[2])
+            bottomRow.addArrangedSubview(imagePickerbuttons[3])
             return
         }
     }
     
+    // Allows to select photo from the phone's library
     @objc private func addPhotoFromLibrary(sender: UIButton) {
         buttonBuffer = sender
         let imagePickerController = UIImagePickerController()
         imagePickerController.delegate = self
         present(imagePickerController, animated: true, completion: nil)
         imagePickerController.sourceType = .photoLibrary
-        
-        
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        
         if let originalImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             buttonBuffer.setImage(originalImage, for: .normal)
             buttonBuffer.imageView?.contentMode = .scaleAspectFill
@@ -173,7 +171,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         dismiss(animated: true, completion: nil)
     }
     
-    private func makeAddButton(count: Int) -> [UIButton] {
+    // Generates imagePickerButtons for the mainView
+    private func makeImagePickerButton(count: Int) -> [UIButton] {
         var buttons: [UIButton] = []
         for _ in 1...count {
             let button = UIButton()
@@ -186,10 +185,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     private func resetRows() {
-        
         for view in topRow.subviews {
             view.removeFromSuperview()
-            
         }
         
         for view in bottomRow.subviews {
@@ -197,6 +194,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         }
     }
     
+    // Modify swipeLabel and swipeArrowImage according to interface orientation
     @objc private func resetOrientation() {
         var windowInterfaceOrientation: UIInterfaceOrientation? {
             if #available(iOS 13.0, *) {
@@ -207,20 +205,16 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         }
         guard let interfaceOrientation = windowInterfaceOrientation else { return }
         if interfaceOrientation.isPortrait {
-            swipeGestureRecognier.direction = .up
+            swipeGestureRecognizer.direction = .up
             swipeLabel.text = "Swipe up to share"
             swipeArrowImage.image = UIImage(named: "Arrow Up")
             transformAnimation = CGAffineTransform(translationX: 0, y: -UIScreen.main.bounds.height)
         } else {
-            swipeGestureRecognier.direction = .left
+            swipeGestureRecognizer.direction = .left
             swipeLabel.text = "Swipe left to share"
             swipeArrowImage.image = UIImage(named: "Arrow Left")
             transformAnimation = CGAffineTransform(translationX: -UIScreen.main.bounds.width, y: 0)
         }
     }
-    
-    
-    
-    
 }
 
